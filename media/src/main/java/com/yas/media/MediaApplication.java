ebe40050f9711de2aa84d37cf525d87a0f60aa7d
XuXinhNo1 <khaihungpro2005@gmail.com>
2026-04-25 19:11:03 +0700

    add smelly code to test scanner

--- media/src/main/java/com/yas/media/MediaApplication.java
+++ media/src/main/java/com/yas/media/MediaApplication.java
@@ -13,5 +13,23 @@ public class MediaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MediaApplication.class, args);
     }
+    // Hàm cố tình viết sai để test SonarQube
+    public void testSonarQubeFails() {
+        // 1. Lỗi bảo mật: Hardcode mật khẩu (Security Hotspot/Vulnerability)
+        String dbPassword = "my_super_secret_password_123!";
+        
+        // 2. Lỗi Bug: Null Pointer Exception chắc chắn xảy ra
+        String text = null;
+        if (text.equals("hello")) { 
+            System.out.println("Lỗi rồi!");
+        }
+
+        // 3. Lỗi Code Smell: Try-catch rỗng (Nuốt exception)
+        try {
+            int a = 10 / 0;
+        } catch (Exception e) {
+            // Cố tình để trống, SonarQube cực ghét điều này
+        }
+    }
 
 }
